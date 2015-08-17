76af8e491e4593a2978053ae67d481e79c47599f
Kevin Lau <thekevlau@gmail.com>
2015-08-16 23:42:17 -0400

    I added more routes for redirect and rendering templates after importing redirect and render_template modules from flask

--- webapp/__init__.py
+++ webapp/__init__.py
@@ -3,7 +3,7 @@ WSGI webapp using Flask
 """
 
 from flask import Flask
-
+from flask import redirect, render_template
 
 app = Flask(__name__)
 
@@ -11,9 +11,15 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     """Homepage"""
-
     return "Hello, linux.conf.au"
 
+@app.route('/a-redirect')
+def a_redirect():
+    return redirect(some_url)
+
+@app.route('/a-template')
+def a_template():
+    return render_template('template.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
